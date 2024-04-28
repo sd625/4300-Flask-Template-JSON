4efe35383a697696e9088fde9758eec32de921a8
Imani Finkley <79882520+imanif@users.noreply.github.com>
2024-04-28 17:00:15 -0400

    changing sentiment classifier and vectorizer

--- backend/sentiment_rank.py
+++ backend/sentiment_rank.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import json
 from sklearn.metrics import classification_report
@@ -17,8 +18,9 @@ X_train, X_test, y_train, y_test = train_test_split(
     reviews_data, ratings, test_size=0.15, random_state=42
 )
 
-# count vectorizer
-vectorizer = CountVectorizer()
+# vectorizer
+count_vectorizer = CountVectorizer()
+vectorizer = TfidfVectorizer()
 X_train_vectorized = vectorizer.fit_transform(X_train)
 X_test_vectorized = vectorizer.transform(X_test)
 
@@ -29,6 +31,15 @@ nb_classifier.fit(X_train_vectorized, y_train)
 # glance at nb accuracy
 nb_pred = nb_classifier.predict(X_test_vectorized)
 nb_report = classification_report(y_test, nb_pred)
+
+# random forest classifier is better at accounting for non linear relationships
+rf_classifer = RandomForestClassifier()
+rf_classifer.fit(X_train_vectorized, y_train)
+
+# glance at rf accuracy
+rf_pred = rf_classifer.predict(X_test_vectorized)
+rf_report = classification_report(y_test, rf_pred)
+
 
 # program and median rating
 group = data.groupby("program")["review_rating"].median()
@@ -41,7 +52,7 @@ def sentiment_ranking(
     query,
     filtered_programs,  # programs from edit distance
     vectorizer=vectorizer,
-    classifier=nb_classifier,
+    classifier=rf_classifier,
 ):
     if query == "": return filtered_programs
     q_vec = vectorizer.transform([query])
